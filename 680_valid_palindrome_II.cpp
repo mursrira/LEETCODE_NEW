b430f30269b59568d186aad073569d7efea4ebc9
muralikrishnasai36@gmail.com <muralikrishnasai36@gmail.com>
2022-05-15 19:40:35 -0700

    added problem 680

---
+++ 680_valid_palindrome_II.cpp
@@ -0,0 +1,42 @@
+class Solution {
+    public:
+        bool validPalindrome(string s) {
+        
+            int start = 0;
+            int end = s.length()-1;
+            int len_s = s.length();
+            
+            while(start < end){
+                
+                if(s[start] != s[end]){
+                                        
+                    return (helper(s.substr(start+1,len_s-1)) or helper(s.substr(start,len_s-1)));
+                    
+                }
+                
+                       
+                start += 1;
+                end -= 1;
+                len_s -= 2;
+            }
+            
+            return true;
+            
+        }
+    
+        bool helper(string s){
+
+            int start = 0;
+            int end = s.length()-1;
+            
+            while(start < end){
+                if(s[start] != s[end]){
+                    return false;
+                }
+                start += 1;
+                end -= 1;
+            }
+            return true;
+        }
+    
+};
